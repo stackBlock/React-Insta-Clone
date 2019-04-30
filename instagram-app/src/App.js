089b2cc9d67d78d4a1d30fb958e21349e2b61932
anthony <stackchain@gmail.com>
2019-04-30 10:53:20 -0400

    commit- more design work done

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,39 +1,23 @@
 import React from "react";
-import { Container, Row, Col, Jumbotron, Button, Navbar } from "reactstrap";
+import { Container, Row, Col, Navbar } from "reactstrap";
 import toppic from "./ig_search_bar.png";
+import heart from "./like.png";
+import chat from "./chat.png";
 import "./App.css";
 import NavbarBrand from "reactstrap/lib/NavbarBrand";
 import dummyData from "./dummy-data";
 
-function PostContainer(props) {
+function CommentSection(props) {
   return (
     <div>
-      <p>name: {props.gram.username}</p>
-
-      <Row className="middlewidth toppart">
-        <Col>
-          <Navbar>
-            <NavbarBrand>
-              image
-              <NavbarBrand className="navbarspace">account name</NavbarBrand>
-            </NavbarBrand>
-          </Navbar>
-        </Col>
-      </Row>
-      <Row className="middlewidth">
-        <img
-          src={
-            "https://tk-assets.lambdaschool.com/69cf901b-f96d-466e-a745-ff2a01effac9_philz-image.jpg"
-          }
-          alt="main pic"
-        />
-      </Row>
       <Row className="under-image">
         <Col>
           <Navbar>
             <NavbarBrand>
-              image
-              <NavbarBrand className="navbarspace">image</NavbarBrand>
+              <img className="heart-img" src={heart} alt="heart" />
+              <NavbarBrand className="navbarspace">
+                <img className="heart-img" src={chat} alt="heart" />
+              </NavbarBrand>
             </NavbarBrand>
           </Navbar>
           <Col className="neg-margin-top">999 likes</Col>
@@ -52,6 +36,46 @@ function PostContainer(props) {
           </Navbar>
         </Col>
       </Row>
+    </div>
+  );
+}
+
+function MainImage(props) {
+  return (
+    <div>
+      <Row className="middlewidth">
+        <img src={props.mainImg} alt="main pic" />
+      </Row>
+    </div>
+  );
+}
+
+function Header(props) {
+  return (
+    <div>
+      {console.log(props.logo)}
+      {console.log(props.name)}
+
+      <Row className="middlewidth toppart">
+        <Col>
+          <Navbar>
+            <NavbarBrand>
+              <img className="logo-img" src={props.logo} alt="logo" />
+              <NavbarBrand className="navbarspace">{props.name}</NavbarBrand>
+            </NavbarBrand>
+          </Navbar>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+function PostContainer(props) {
+  return (
+    <div>
+      <Header logo={props.gram.thumbnailUrl} name={props.gram.username} />
+      <MainImage mainImg={props.gram.imageUrl} />
+      <CommentSection />
     </div>
   );
 }
@@ -80,7 +104,7 @@ class App extends React.Component {
 <Col xs="1">.col</Col>
 </Row> */}
           {this.state.myData.map(gram => (
-            <PostContainer gram={gram} />
+            <PostContainer gram={gram} key={gram[1]} />
           ))}
         </Container>
       </div>
